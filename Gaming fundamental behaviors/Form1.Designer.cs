dd8f4cf860d58ee0857bd8372c061ef39ec5ad96
Mikhail Bounoung <67975897+Merca0va@users.noreply.github.com>
2020-11-20 12:36:50 +0000

    Changes Imported

---
+++ Gaming fundamental behaviors/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Gaming_fundamental_behaviors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.animateImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arrowKeysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mouseClickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotateImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bufferingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.singleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.doubleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(67, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(322, 237);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.animateImageToolStripMenuItem,
+            this.changeImageToolStripMenuItem,
+            this.bufferingToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // animateImageToolStripMenuItem
+            // 
+            this.animateImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveImageToolStripMenuItem,
+            this.rotateImageToolStripMenuItem,
+            this.resizeImageToolStripMenuItem});
+            this.animateImageToolStripMenuItem.Name = "animateImageToolStripMenuItem";
+            this.animateImageToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.animateImageToolStripMenuItem.Text = "Animate Image";
+            // 
+            // moveImageToolStripMenuItem
+            // 
+            this.moveImageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arrowKeysToolStripMenuItem,
+            this.mouseClickToolStripMenuItem});
+            this.moveImageToolStripMenuItem.Name = "moveImageToolStripMenuItem";
+            this.moveImageToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.moveImageToolStripMenuItem.Text = "Move Image";
+            // 
+            // arrowKeysToolStripMenuItem
+            // 
+            this.arrowKeysToolStripMenuItem.Name = "arrowKeysToolStripMenuItem";
+            this.arrowKeysToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.arrowKeysToolStripMenuItem.Text = "Arrow Keys";
+            this.arrowKeysToolStripMenuItem.Click += new System.EventHandler(this.arrowKeysToolStripMenuItem_Click);
+            // 
+            // mouseClickToolStripMenuItem
+            // 
+            this.mouseClickToolStripMenuItem.Name = "mouseClickToolStripMenuItem";
+            this.mouseClickToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.mouseClickToolStripMenuItem.Text = "Mouse Click";
+            this.mouseClickToolStripMenuItem.Click += new System.EventHandler(this.mouseClickToolStripMenuItem_Click);
+            // 
+            // rotateImageToolStripMenuItem
+            // 
+            this.rotateImageToolStripMenuItem.Name = "rotateImageToolStripMenuItem";
+            this.rotateImageToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.rotateImageToolStripMenuItem.Text = "Rotate Image";
+            this.rotateImageToolStripMenuItem.Click += new System.EventHandler(this.rotateImageToolStripMenuItem_Click);
+            // 
+            // resizeImageToolStripMenuItem
+            // 
+            this.resizeImageToolStripMenuItem.Name = "resizeImageToolStripMenuItem";
+            this.resizeImageToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.resizeImageToolStripMenuItem.Text = "Resize Image";
+            this.resizeImageToolStripMenuItem.Click += new System.EventHandler(this.resizeImageToolStripMenuItem_Click);
+            // 
+            // changeImageToolStripMenuItem
+            // 
+            this.changeImageToolStripMenuItem.Name = "changeImageToolStripMenuItem";
+            this.changeImageToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.changeImageToolStripMenuItem.Text = "Change Image";
+            this.changeImageToolStripMenuItem.Click += new System.EventHandler(this.changeImageToolStripMenuItem_Click);
+            // 
+            // bufferingToolStripMenuItem
+            // 
+            this.bufferingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.singleToolStripMenuItem,
+            this.doubleToolStripMenuItem});
+            this.bufferingToolStripMenuItem.Name = "bufferingToolStripMenuItem";
+            this.bufferingToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.bufferingToolStripMenuItem.Text = "Buffering";
+            // 
+            // singleToolStripMenuItem
+            // 
+            this.singleToolStripMenuItem.Name = "singleToolStripMenuItem";
+            this.singleToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.singleToolStripMenuItem.Text = "Single";
+            this.singleToolStripMenuItem.Click += new System.EventHandler(this.singleToolStripMenuItem_Click);
+            // 
+            // doubleToolStripMenuItem
+            // 
+            this.doubleToolStripMenuItem.Name = "doubleToolStripMenuItem";
+            this.doubleToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.doubleToolStripMenuItem.Text = "Double";
+            this.doubleToolStripMenuItem.Click += new System.EventHandler(this.doubleToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Object Behavior";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem animateImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arrowKeysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mouseClickToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotateImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resizeImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bufferingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem singleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem doubleToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Gaming fundamental behaviors/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gaming_fundamental_behaviors
+{
+    public partial class Form1 : Form
+    {
+
+        enum Direction
+        {
+            Left, Right, Up, Down
+        }
+
+        int x_Position, y_Position;
+        int my_Height, my_Width;
+        int max_Height = 500, max_Width = 700;
+        int min_Height = 50, min_Width = 50;
+        Boolean size_is_expanding = true;
+        Boolean arrow_keys_active = true;
+        string animation_mode = "none of these"; // move or size or spin and none of these to stop the animations.
+        Direction my_direction;
+        Image img;
+
+        private void arrowKeysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            arrow_keys_active = true;
+            pictureBox1.Height = 300;
+            pictureBox1.Width = 300;
+            pictureBox1.Top = 100;
+            pictureBox1.Left = 100;
+            animation_mode = "move";
+        }
+
+        private void mouseClickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            arrow_keys_active = false;
+            pictureBox1.Height = 300;
+            pictureBox1.Width = 300;
+            pictureBox1.Top = 100;
+            pictureBox1.Left = 100;
+            animation_mode = "move";
+        }
+
+        private void rotateImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Height = 300;
+            pictureBox1.Width = 300;
+            pictureBox1.Top = 100;
+            pictureBox1.Left = 100;
+            animation_mode = "spin";
+        }
+
+        private void resizeImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Height = 300;
+            pictureBox1.Width = 300;
+            pictureBox1.Top = 100;
+            pictureBox1.Left = 100;
+            animation_mode = "size";
+        }
+
+        private void changeImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void singleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DoubleBuffered = false;
+        }
+
+        private void doubleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            animation_mode = "none of these";
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (arrow_keys_active)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    my_direction = Direction.Up;
+                }
+
+                else if (e.KeyCode == Keys.Down)
+                {
+                    my_direction = Direction.Down;
+                }
+
+                else if (e.KeyCode == Keys.Left)
+                {
+                    my_direction = Direction.Left;
+                }
+
+                else if (e.KeyCode == Keys.Right)
+                {
+                    my_direction = Direction.Right;
+                }
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Picture jumps to mouse location when left click is pressed.
+            if (e.Button == MouseButtons.Left && arrow_keys_active == false)
+            {
+                pictureBox1.Left = e.X;
+                pictureBox1.Top = e.Y;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Height = 300;
+            pictureBox1.Width = 300;
+            pictureBox1.Top = 100;
+            pictureBox1.Left = 100;
+            my_direction = Direction.Right;
+            img = pictureBox1.Image;
+        }
+
+      
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (animation_mode == "size")
+            {
+
+                if (my_Height < max_Height && my_Width < max_Width && size_is_expanding)
+                {
+                    my_Height = my_Height + 10;
+                    my_Width = my_Width + 10;
+                }
+
+                else if (my_Height == max_Height && my_Width < max_Width && size_is_expanding)
+                {
+                    my_Width = my_Width + 10;
+                }
+
+                else if (my_Height == max_Height && my_Width == max_Width)
+                {
+                    my_Height = my_Height - 10;
+                    my_Width = my_Width - 10;
+                    size_is_expanding = false;
+                }
+
+                else if (my_Height > min_Height && my_Width > min_Width)
+                {
+                    my_Width = my_Width - 10;
+                    my_Height = my_Height - 10;
+                }
+
+                else if (my_Height == min_Height && my_Width > min_Width)
+                {
+                    my_Width = my_Width - 10;
+                }
+
+                else if (my_Height == min_Height && my_Width == min_Width)
+                {
+                    my_Height = my_Height + 10;
+                    my_Width = my_Width + 10;
+                    size_is_expanding = true;
+                }
+
+                pictureBox1.Height = my_Height;
+                pictureBox1.Width = my_Width;
+            }
+
+            else if (animation_mode == "move" && arrow_keys_active == true) // if (size_or_movement == "move")
+            {
+                if (my_direction == Direction.Up)
+                {
+                    y_Position = y_Position - 10;
+                }
+                else if (my_direction == Direction.Down)
+                {
+                    y_Position = y_Position + 10;
+                }
+                else if (my_direction == Direction.Right)
+                {
+                    x_Position = x_Position + 10;
+                }
+                else if (my_direction == Direction.Left)
+                {
+                    x_Position = x_Position - 10;
+                }
+
+                pictureBox1.Left = x_Position;
+                pictureBox1.Top = y_Position;
+            }
+
+            else if (animation_mode == "spin")
+            {
+                img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = img;
+            }
+
+            Invalidate();
+        }
+    }
+}
